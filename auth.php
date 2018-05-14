9b5b9c7b669f7ec3470e3ba33408b918f61caf8a
Vitor Hugo <vitorzcomputacao@gmail.com>
2018-05-14 00:55:54 -0300

    Validation of a simple LOGIN

--- auth.php
+++ auth.php
@@ -1,8 +1,23 @@
 <?php
 
 
-echo var_dump($_POST);
+try{
+    $conn = new PDO("mysql:host=localhost;dbname=parking-control", "root", "");
 
-echo $_POST["user"];
-echo $_POST["password"];
+    $stmt = $conn->prepare("SELECT name,password FROM users where name = ? and password = ?");
+    $stmt->execute(array($_POST['user'],$_POST['password']));
+    $result = $stmt->fetchAll();
+
+    if($result){
+        print 'Bem vindo';
+    }else{
+        print 'Usuário ou senha inválidos';
+    }
+    $conn = null;
+
+}catch(PDOException $e){
+    print "Falhou  a conexão com o banco de dados ". $e->getMessage(). "<br/>";
+    
+}
+
 ?>
--- utils.php
+++ utils.php
@@ -1,4 +1,3 @@
 <link rel="stylesheet" type="text/css" href="\parking-control\libs\boostrap\css\bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="\parking-control\css\login.css">
 <script src="libs\jquery\jquery-3.3.1.slim.min.js"></script>
 <script src="\parking-control\libs\boostrap\js\bootstrap.min.js"></script>
