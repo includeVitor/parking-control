76885fb048f9b053e251ecf4e2b3bae3ee76292d
Vitor Hugo <vitorzcomputacao@gmail.com>
2018-05-17 01:45:37 -0300

    Url amigável, menu definido.

--- index.php
+++ index.php
@@ -12,21 +12,105 @@ if(!$_SESSION) exit;
     <?php include 'utils.php'; ?>
     <link rel="stylesheet" type="text/css" href="\parking-control\libs\boostrap\css\bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="\parking-control\css\index.css">
+    <script src="\parking-control\scripts\index.js"></script>
     
 </head>
-<body>
-    <header>
-        <a id="glyphUser">
-            <i class="glyphicon glyphicon-user"></i>
-        </a>
-        <a id="glyphLogOut">
-          <i class="glyphicon glyphicon-log-out"></i>
-          <label for="get-out">Sair</label>
-        </a>
-    </header>
-    <label for="welcome">Bem-vindo, "O sucesso normalmente vem para quem está ocupado demais para procurar por ele" – Henry David Thoreau </label>
+    <body>
+        <div class="wrapper">
+            <nav id="sidebar">
+                <div class="sidebar-header">
+                    <h3>Estacionamento inteligente</h3>
+                    <strong>EI</strong>
+                </div>
+                <ul class="list-unstyled components">
+                    <li class="active">
+                        <a href="#vehicles-control" data-toggle="collapse" aria-expanded="false">
+                            <i class="glyphicon glyphicon-usd"></i>
+                            Controle de Veículos
+                        </a>
+                        <ul class="collapse list-unstyled" id="vehicles-control">
+                            <li><a href="#">Cadastro</a></li>
+                            <li><a href="#">Consulta</a></li>
+                            <li><a href="#">Exclusão</a></li>
+                            <li><a href="#">Edição</a></li>
+                        </ul>
+                    </li>
+                    <li class="active">
+                        <a href="#custumer-control" data-toggle="collapse" aria-expanded="false">
+                            <i class="glyphicon glyphicon-user"></i>
+                            Controle de Clientes
+                        </a>
+                        <ul class="collapse list-unstyled" id="custumer-control">
+                            <li><a href="#">Cadastro</a></li>
+                            <li><a href="#">Consulta</a></li>
+                            <li><a href="#">Exclusão</a></li>
+                            <li><a href="#">Edição</a></li>
+                        </ul>
+                    </li>
+                    <li class="active">
+                        <a href="#analytics" data-toggle="collapse" aria-expanded="false">
+                            <i class="glyphicon glyphicon-signal"></i>
+                            Analytics
+                        </a>
+                        <ul class="collapse list-unstyled" id="analytics">
+                            <li><a href="#">Gráfico de Clientes</a></li>
+                            <li><a href="#">Gráfico de Veículos</a></li>
+                            <li><a href="#">Gráfico Clientes X Veículos</a></li>
+                        </ul>
+                    </li>
+                    <li class="active">
+                        <a href="#admin" data-toggle="collapse" aria-expanded="false">
+                            <i class="glyphicon glyphicon-signal"></i>
+                            Painel do Admnistrador
+                        </a>
+                        <ul class="collapse list-unstyled" id="admin">
+                            <li><a href="\parking-control\modules\admin-panel\create-customers.php">Cadastrar Usuários</a></li>
+                            <li><a href="#">Consultar Usuários</a></li>
+                            <li><a href="#">Excluir Usuários</a></li>
+                            <li><a href="#">Editar Usuários</a></li>
+                            <li><a href="#">Permissões do Usuário</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="glyphicon glyphicon-send"></i>
+                            Contato
+                        </a>
+                    </li>
+                </ul>
 
-    <footer>
-    </footer>
-</body>
+            </nav>
+            <div id="content">
+
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid">
+
+                        <div class="navbar-header">
+                            <button type="button" id="sidebarCollapse" class="btn btn-info navbar-btn">
+                                <i class="glyphicon glyphicon-align-left"></i>
+                                <span>Menu</span>
+                            </button>
+                        </div>
+
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li>
+                                    <a href="#"> <i class="glyphicon glyphicon-log-out"></i>Sair</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <?php
+                    $Url[1] = (empty($Url[1]) ? null : $Url[1]);
+                    if(file_exists('C:/xampp/htdocs/parking-control/modules/admin-panel/'.$Url[0].'.php'))
+                       require 'C:/xampp/htdocs/parking-control/modules/admin-panel/'.$Url[0].'.php';
+                ?>
+               <!-- <h2>Cadastrar Usuário</h2>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+-->
+            </div>
+        </div>
+    </body>
 </html>
--- utils.php
+++ utils.php
@@ -1,3 +1,12 @@
 <script src="libs\jquery\jquery-3.3.1.min.js"></script>
 <script src="libs\jquery\jquery.form.min.js"></script>
 <script src="\parking-control\libs\boostrap\js\bootstrap.min.js"></script>
+<?php
+
+$getUrl = strip_tags(trim(filter_input(INPUT_GET,'url',FILTER_DEFAULT)));
+$setUrl = (empty($getUrl)? 'index': $getUrl);
+$Url = explode('/',$setUrl);
+
+
+
+?>
